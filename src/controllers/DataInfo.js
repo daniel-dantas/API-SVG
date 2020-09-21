be423b53bdd3084a6caec77ad1a195ae45b7bc1e
daniel-dantas <daniel.dantas.developer@gmail.com>
2020-09-21 11:17:12 -0300

    Mudando retorno da rota getCitys

--- src/controllers/DataInfo.js
+++ src/controllers/DataInfo.js
@@ -14,14 +14,11 @@ class DataInfoController {
 
     const { estado } = request.params;
 
-    pool.query('select nome from municipio where estado ilike $1', [estado],(error, results) => {
+    pool.query('SELECT nome, area FROM municipio WHERE estado ilike $1', [estado],(error, results) => {
       if (error) {
         throw error;
       }
-      const res = results.rows.map(estado => {
-        return estado.nome;
-      });
-      response.status(200).json(res);
+      response.status(200).json(results.rows);
     });
   }
 
